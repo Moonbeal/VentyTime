cbc92ee424491118282dd1f4fb37ffb26af1952e
VentyTime Developer <venty.time@example.com>
2024-12-23 13:15:09 +0200

    Checkpoint 12 - Updated Program.cs with enhanced security and CORS configuration

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
